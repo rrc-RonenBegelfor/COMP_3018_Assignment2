9e854f700e9999e5e94c82b7b2ed452d43ea4aff
Ronen Begelfor <rbegelfor@rrc.ca>
2025-10-01 18:49:38 -0500

    Added AAA for routes.

--- test/branchRoutes.test.ts
+++ test/branchRoutes.test.ts
@@ -20,16 +20,20 @@ describe("Branch Routs", () => {
 
     describe("GET /api/v1/branches/", () => {
         it("should call getAllBranches controller", async () => {
+            // Arrange
             await request(app).get("/api/v1/branches/");
+
+            // Act & Assert
             expect(branchController.getAllBranches).toHaveBeenCalled();
         });
 
         it("should call getAllBranches and handle failure", async () => {
-
+            // Arrange
             (branchController.getAllBranches as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).send({}); 
             });
 
+            // Act & Assert
             const response: response.Response = await request(app).get("/api/v1/branches/");
             expect(response.status).toBe(HTTP_STATUS.INTERNAL_SERVER_ERROR);
         });
@@ -37,6 +41,7 @@ describe("Branch Routs", () => {
 
     describe("POST /api/v1/branches/", () => {
         it("should call createBranch controller with valid data", async () => {
+            // Arrange
             const mockBranch: Branch = {
                 id: 1,
                 name: "Test Name",
@@ -45,22 +50,27 @@ describe("Branch Routs", () => {
             };
 
             await request(app).post("/api/v1/branches/").send(mockBranch);
+
+            // Act & Assert
             expect(branchController.createBranch).toHaveBeenCalled();
         });
 
         it("should call createBranch and handle failure", async () => {
-
+            // Arrange
             (branchController.createBranch as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.BAD_REQUEST).send({}); 
             });
 
             const response: response.Response = await request(app).post("/api/v1/branches/");
+
+            // Act & Assert
             expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
         });
     });
 
     describe("PUT /api/v1/branches/:id", () => {
         it("should call updateBranch controller with valid data", async () => {
+            // Arrange
             const mockBranchUpdate: Branch = {
                 id: 1,
                 name: "Test Name",
@@ -69,49 +79,62 @@ describe("Branch Routs", () => {
             };
 
             await request(app).put("/api/v1/branches/1").send(mockBranchUpdate);
+
+            // Act & Assert
             expect(branchController.updateBranch).toHaveBeenCalled();
         });
 
         it("should call updateBranch and handle failure", async () => {
-
+            // Arrange
             (branchController.updateBranch as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.NOT_FOUND).send({}); 
             });
 
             const response: response.Response = await request(app).put("/api/v1/branches/-1");
+
+            // Act & Assert
             expect(response.status).toBe(HTTP_STATUS.NOT_FOUND);
         });
     });
 
     describe("DELETE /api/v1/branches/:id", () => {
         it("should call deleteBranch controller for the valid ID", async () => {
+            // Arrange
             await request(app).delete("/api/v1/branches/1");
+
+            // Act & Assert
             expect(branchController.deleteBranch).toHaveBeenCalled();
         });
 
         it("should call deleteBranch and handle failure", async () => {
-
+            // Arrange
             (branchController.updateBranch as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.NOT_FOUND).send({}); 
             });
 
             const response: response.Response = await request(app).put("/api/v1/branches/-1");
+
+            // Act & Assert
             expect(response.status).toBe(HTTP_STATUS.NOT_FOUND);
         });
     });
 
     describe("GET /api/v1/branches/:id", () => {
         it("shoud call getBranchById controller with provided ID", async () => {
+            // Arrange
             await request(app).get("/api/v1/branches/1");
+
+            // Act & Assert
             expect(branchController.getBranchById).toHaveBeenCalled();
         });
 
         it("should call getBranchById and handle failure", async () => {
-
+            // Arrange
             (branchController.getBranchById as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.NOT_FOUND).send({}); 
             });
 
+            // Act & Assert
             const response: response.Response = await request(app).get("/api/v1/branches/-1");
             expect(response.status).toBe(HTTP_STATUS.NOT_FOUND);
         });
--- test/employeeRoutes.test.ts
+++ test/employeeRoutes.test.ts
@@ -21,24 +21,30 @@ describe("Employee Routs", () => {
 
     describe("GET /api/v1/employees/", () => {
         it("should call getAllEmployees controller", async () => {
+            // Arrange
             await request(app).get("/api/v1/employees/");
+
+            // Act & Assert
             expect(employeeController.getAllEmployees).toHaveBeenCalled();
         });
         
         // https://stackoverflow.com/questions/49835264/how-to-properly-make-mock-throw-an-error-in-jest
         it("should call getAllEmployees and handle failure", async () => {
-
+            // Arrange
             (employeeController.getAllEmployees as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).send({}); 
             });
 
             const response: response.Response = await request(app).get("/api/v1/employees/");
+
+            // Act & Assert
             expect(response.status).toBe(HTTP_STATUS.INTERNAL_SERVER_ERROR);
         });
     });
 
     describe("POST /api/v1/employees/", () => {
         it("should call createEmployee controller with valid data", async () => {
+            // Arrange
             const mockEmployee: Employee = {
                 id: 1,
                 name: "Test Name",
@@ -50,23 +56,28 @@ describe("Employee Routs", () => {
             };
 
             await request(app).post("/api/v1/employees/").send(mockEmployee);
+
+            // Act & Assert
             expect(employeeController.createEmployee).toHaveBeenCalled();
         });
 
         it("should call createEmployee and handle failure", async () => {
-
+            // Arrange
             (employeeController.createEmployee as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.BAD_REQUEST).send({}); 
             });
 
             // Could add an empty array/object and send as an empty body, both would return '400'.
             const response: response.Response = await request(app).post("/api/v1/employees/");
+
+            // Act & Assert
             expect(response.status).toBe(HTTP_STATUS.BAD_REQUEST);
         });
     });
 
     describe("PUT /api/v1/employees/:id", () => {
         it("should call updateEmployee controller with valid data", async () => {
+            // Arrange
             const mockEmployeeUpdate: Employee = {
                 id: 1,
                 name: "Test Name",
@@ -78,67 +89,86 @@ describe("Employee Routs", () => {
             };
 
             await request(app).put("/api/v1/employees/1").send(mockEmployeeUpdate);
+
+            // Act & Assert
             expect(employeeController.updateEmployee).toHaveBeenCalled();
         });
 
         it("should call updateEmployee and handle failure", async () => {
-
+            // Arrange
             (employeeController.updateEmployee as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.NOT_FOUND).send({}); 
             });
 
             const response: response.Response = await request(app).put("/api/v1/employees/-1");
+
+            // Act & Assert
             expect(response.status).toBe(HTTP_STATUS.NOT_FOUND);
         });
     });
 
     describe("DELETE /api/v1/employees/:id", () => {
         it("should call deleteEmployee controller for the valid ID", async () => {
+            // Arrange
             await request(app).delete("/api/v1/employees/1");
+
+            // Act & Assert
             expect(employeeController.deleteEmployee).toHaveBeenCalled();
         });
 
         it("should call deleteEmployee and handle failure", async () => {
-
+            // Arrange
             (employeeController.deleteEmployee as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.NOT_FOUND).send({}); 
             });
 
             const response: response.Response = await request(app).put("/api/v1/employees/-1");
+
+            // Act & Assert
             expect(response.status).toBe(HTTP_STATUS.NOT_FOUND);
         });
     });
 
     describe("GET /api/v1/employees/:id", () => {
         it("shoud call getEmployeeById controller with provided ID", async () => {
+            // Arrange
             await request(app).get("/api/v1/employees/1");
+
+            // Act & Assert
             expect(employeeController.getEmployeeById).toHaveBeenCalled();
         });
 
         it("should call getEmployeeById and handle failure", async () => {
-
+            // Arrange
             (employeeController.getEmployeeById as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.NOT_FOUND).send({}); 
             });
 
             const response: response.Response = await request(app).get("/api/v1/employees/-1");
+
+            // Act & Assert
             expect(response.status).toBe(HTTP_STATUS.NOT_FOUND);
         });
     });
 
     describe("GET /api/v1/employees?department=", () => {
         it("should call getAllEmployees controller has a department query", async () => {
+            // Arrange
             await request(app).get("/api/v1/employees?department=test");
+
+            // Act & Assert
             expect(employeeController.getAllEmployees).toHaveBeenCalled();
         });
 
         it("should call getAllEmployees with department query and handle failure", async () => {
-
+            // Arrange
             (employeeController.getAllEmployees as jest.Mock).mockImplementation((req, res) => {
                return res.status(HTTP_STATUS.NOT_FOUND).send({}); 
             });
 
             const response: response.Response = await request(app).get("/api/v1/employees?department=test");
+
+            // Act & Assert
             expect(response.status).toBe(HTTP_STATUS.NOT_FOUND);
         });
     });
