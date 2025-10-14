3bb4bd170e425b57493ccb41ad04c905e5583cfd
Ronen Begelfor <rbegelfor@rrc.ca>
2025-10-14 02:19:55 -0500

    Added branchSchema and joi validation to future create functionality.

---
+++ src/api/v1/validation/branchValidation.ts
@@ -0,0 +1,30 @@
+import Joi from "joi";
+import { BranchRequestModel } from "../models/branchRequestModel";
+
+export const branchSchemas = {
+    create: {
+        body: Joi.object<BranchRequestModel>({
+            name: Joi.string().required().min(3).messages({
+                "string.base": "Branch name must be a type of string",
+                "string.empty": "Branch name cannot be empty",
+                "string.min": `Branch name should have a minimum length of 3`,
+                "any.required": "Brach name is required",
+            }),
+            address: Joi.string().required().min(3).messages({
+                "string.base": "Branch address must be a type of string",
+                "string.empty": "Branch address cannot be empty",
+                "string.min": `Branch address should have a minimum length of 3`,
+                "any.required": "Brach address is required",
+            }),
+            phone: Joi.string().required().min(3).messages({
+                "string.base": "Branch phone must be a type of string",
+                "string.empty": "Branch phone cannot be empty",
+                "string.min": `Branch phone should have a minimum length of 3`,
+                "any.required": "Brach phone is required",
+            }),
+        }),
+    },
+    update: {
+
+    },
+};
