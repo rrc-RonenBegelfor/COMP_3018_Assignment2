767d84e294e0e8945bdc2dc11deb71ee04d58490
Ronen Begelfor <rbegelfor@rrc.ca>
2025-10-14 02:26:36 -0500

    Added update schema and joi functionality to branchSchemas.

--- src/api/v1/validation/branchValidation.ts
+++ src/api/v1/validation/branchValidation.ts
@@ -25,6 +25,25 @@ export const branchSchemas = {
         }),
     },
     update: {
-
+        body: Joi.object<BranchRequestModel>({
+            name: Joi.string().required().min(3).messages({
+                "string.base": "Branch name must be a type of string",
+                "string.empty": "Branch name cannot be empty",
+                "string.min": `Branch name should have a minimum length of 3`,
+                "any.required": "Brach name is required",
+            }),
+            address: Joi.string().required().min(3).messages({
+                "string.base": "Branch address must be a type of string",
+                "string.empty": "Branch address cannot be empty",
+                "string.min": `Branch address should have a minimum length of 3`,
+                "any.required": "Brach address is required",
+            }),
+            phone: Joi.string().required().min(3).messages({
+                "string.base": "Branch phone must be a type of string",
+                "string.empty": "Branch phone cannot be empty",
+                "string.min": `Branch phone should have a minimum length of 3`,
+                "any.required": "Brach phone is required",
+            }),
+        }).min(1),
     },
 };
